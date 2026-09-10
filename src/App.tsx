c3b48cf6fd4121a42c2ae2d9eb735124912714a0
Paulo da Rocha Teixeira <circuitointernoproducoes@gmail.com>
2026-09-10 17:03:35 +0100

    Fix: Corrigir URL de stream da Radio Marcoense

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,7 @@ const STREAMS: Record<AudioSource, string> = {
     'https://azuracast.rhoster.pt/listen/circuito_interno/radio.mp3',
 
   marcoense:
-    'https://stream.ips.pt/marcoense/live',
+    'https://stream.ips.pt/marcoense',
 };
 
 const NOW_PLAYING_URL =
